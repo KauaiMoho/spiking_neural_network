a0113b4c6dd1780a0245af396989045b22e4b764
KauaiMoho <113269300+KauaiMoho@users.noreply.github.com>
2025-09-07 12:01:56 -0400

    remove vectors for future compat

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,6 +1,5 @@
 #ifndef MATRIX_H
 #define MATRIX_H
-#include <vector>
 using namespace std;
 
 class Matrix {
@@ -8,24 +7,26 @@ class Matrix {
 private:
 
     //flattened approach
-    vector<float> data;
-    vector<int> dims;
-    vector<int> dists;
-    int get_idx(vector<int> pos);
+    float* data;
+    int* dims;
+    int* dists;
+    int get_idx(int* pos);
+    int dim_len;
+    float data_len;
     
     
 public:
 
-    Matrix(vector<int> dims, vector<float> data);
-    Matrix(vector<int> dims);
-    Matrix matmul(vector<int> this_axes, Matrix other, vector<int> other_axes);
+    Matrix(int* dims_n, int dim_len, float* data_n, int data_len);
+    Matrix(int* dims_n, int dim_len, int val);
+    Matrix matmul(int* this_axes, int len_this_a, Matrix other, int*other_axes, int len_other_a);
     void scmul(float s);
     void add(Matrix a);
     void subtract(Matrix a);
     void transpose();
-    float get(vector<int> pos);
-    void set(vector<int> pos, float val);
-    vector<int> get_full_dims();
+    float get(int* pos);
+    void set(int* pos, float val);
+    int* get_full_dims();
     int get_dims_index(int i);
     float get_index(int i);
 };
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -1,82 +1,120 @@
 #include "Matrix.h"
 using namespace std;
+#include <stdexcept>
+#include <cstdlib>
 
-Matrix::Matrix(vector<int> dims, vector<float> data) : dims(dims), data(data) {
+Matrix::Matrix(int* dims_n, int dim_len, float* data_n, int data_len) : dim_len(dim_len), data_len(data_len) {
     //Define the dists var here
+    if (dim_len == 0) throw invalid_argument("Matrix dimensions cannot be empty!");
+    dists = (int*) malloc(dim_len * sizeof(int));
 
-    if (dims.empty()) throw std::invalid_argument("Matrix dimensions cannot be empty!");
+    if (dists == nullptr) {
+        throw invalid_argument("Memory allocation error");
+    }
 
-    dists.resize(dims.size());
+    dims = (int*) malloc(dim_len * sizeof(int));
+
+    if (dims == nullptr) {
+        throw invalid_argument("Memory allocation error");
+    }
+
+    data = (float*) malloc(data_len * sizeof(float));
+
+    if (data == nullptr) {
+        throw invalid_argument("Memory allocation error");
+    }
+
     int pos = 1;
-    for (int i = dims.size()-1; i > 0 ; i--) {
+    for (int i = dim_len - 1; i > 0 ; i--) {
         dists[i] = pos;
+        dims[i] = dims_n[i];
         pos *= dims[i];
+    }
+    dims[0] = dims_n[0];
+    dists[0] = pos;
+    for (int i = 0; i < data_len; i++) {
+        data[i] = data_n[i];
     }
 }
 
-Matrix::Matrix(vector<int> dims) : dims(dims) {
-    //Define the dists var here
+Matrix::Matrix(int* dims_n, int dim_len, int val) : dim_len(dim_len) {
 
-    if (dims.empty()) throw std::invalid_argument("Matrix dimensions cannot be empty!");
+    if (dim_len == 0) throw invalid_argument("Matrix dimensions cannot be empty!");
+    dists = (int*) malloc(dim_len * sizeof(int));
 
-    dists.resize(dims.size());
+    if (dists == nullptr) {
+        throw invalid_argument("Memory allocation error");
+    }
+
+    dims = (int*) malloc(dim_len * sizeof(int));
+
+    if (dims == nullptr) {
+        throw invalid_argument("Memory allocation error");
+    }
+
+    data = (float*) malloc(data_len * sizeof(float));
+
+    if (data == nullptr) {
+        throw invalid_argument("Memory allocation error");
+    }
+
     int pos = 1;
-    for (int i = dims.size()-1; i > 0 ; i--) {
+    for (int i = dim_len - 1; i > 0 ; i--) {
         dists[i] = pos;
+        dims[i] = dims_n[i];
         pos *= dims[i];
     }
-    data.resize(pos*dims[dims.size()-1]);
+    dims[0] = dims_n[0];
+    dists[0] = pos;
+    for (int i = 0; i < data_len; i++) {
+        data[i] = val;
+    }
 }
 
-int Matrix::get_idx(vector<int> pos) {
+int Matrix::get_idx(int* pos) {
     int idx = 0;
-
-    if (pos.size() != dists.size()) throw std::invalid_argument("Invalid position dimensions!");
-
-    for (int i = 0; i < dists.size(); i++) {
+    for (int i = 0; i < dim_len; i++) {
         idx += pos[i]*dists[i];
     }
-
-    if (idx > data.size() || idx < 0) throw std::invalid_argument("Invalid position dimensions!");
-    
+    if (idx > data_len || idx < 0) throw invalid_argument("Invalid position dimensions!");
     return idx;
 }
 
-Matrix Matrix::matmul(vector<int> this_axes, Matrix other, vector<int> other_axes) {
-    if (this_axes.size() != other_axes.size()){
-        throw std::invalid_argument("Invalid axes dimensions!");
+Matrix Matrix::matmul(int* this_axes, int len_this_a, Matrix other, int*other_axes, int len_other_a) {
+    if (len_this_a != len_other_a){
+        throw invalid_argument("Invalid axes dimensions!");
     }
-    for (int i = 0; i < this_axes.size(); i++) {
+    for (int i = 0; i < len_this_a; i++) {
         if (dims[this_axes[i]] != other.get_dims_index(other_axes[i])) {
-            throw std::invalid_argument("Invalid axes values!");
+            throw invalid_argument("Invalid axes values!");
         }
     }
     //implement matrix multiplaction here
 }   
 
 void Matrix::scmul(float s) {
-    for (int i = 0; i < data.size(); i++) {
+    for (int i = 0; i < data_len; i++) {
         data[i] = data[i] * s;
     }
 }
 
 void Matrix::add(Matrix a) {
     if (a.get_full_dims() == dims) {
-        for (int i = 0; i < data.size(); i++) {
+        for (int i = 0; i < dim_len; i++) {
             data[i] = data[i] + a.get_index(i);
         }
     } else {
-        throw std::invalid_argument("Invalid matrix dimensions!");
+        throw invalid_argument("Invalid matrix dimensions!");
     }
 }
 
 void Matrix::subtract(Matrix a) {
     if (a.get_full_dims() == dims) {
-        for (int i = 0; i < data.size(); i++) {
+        for (int i = 0; i < dim_len; i++) {
             data[i] = data[i] - a.get_index(i);
         }
     } else {
-        throw std::invalid_argument("Invalid matrix dimensions!");
+        throw invalid_argument("Invalid matrix dimensions!");
     }
 }
 
@@ -84,28 +122,28 @@ void Matrix::transpose() {
 
 }
 
-float Matrix::get(vector<int> pos) {
+float Matrix::get(int* pos) {
     return data[get_idx(pos)];
 }
 
-void Matrix::set(vector<int> pos, float val) {
+void Matrix::set(int* pos, float val) {
     data[get_idx(pos)] = val;
 }
 
-vector<int> Matrix::get_full_dims() {
+int* Matrix::get_full_dims() {
     return dims;
 }
 
 int Matrix::get_dims_index(int i) {
-    if (i < 0 || i > dims.size()-1){
-        throw std::invalid_argument("Invalid index!");
+    if (i < 0 || i > dim_len-1){
+        throw invalid_argument("Invalid index!");
     }
     return dims[i];
 }
 
 float Matrix::get_index(int i) {
-    if (i < 0 || i > data.size()-1){
-        throw std::invalid_argument("Invalid index!");
+    if (i < 0 || i > dim_len-1){
+        throw invalid_argument("Invalid index!");
     }
     return data[i];
 }
